a653b1de8ae7f15f54e7de63131d59884cdbefd1
Andrew Kozlov <a.kozlov@growapp.biz>
2021-05-05 20:35:49 +0300

    add CardCollectionLayout to ViewController

--- CardsCollectionViewLayoutExample/ViewController.swift
+++ CardsCollectionViewLayoutExample/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	private var cards: [String] = [
+		"First",
+		"Second",
+		"Third"
+	]
+	
+	private let layout = CardsCollectionLayout()
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		view.backgroundColor = .red
+	private lazy var collectionView: UICollectionView = {
+		let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+		let cardSize: CGFloat = 282
+		layout.setupWidthAndInsets(cardWidth: cardSize)
+		collection.backgroundColor = UIColor.lightGray
+		collection.delegate = self
+		collection.dataSource = self
+		collection.decelerationRate = .fast
+		collection.showsHorizontalScrollIndicator = false
+		collection.register(cell: CardCollectionViewCell.self)
+		return collection
+	}()
+	
+	override func loadView() {
+		view = collectionView
 	}
-
 
 }
 
+// MARK: - UICollectionViewDelegate
+
+extension ViewController: UICollectionViewDelegate {
+	
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		guard let cell = collectionView.cellForItem(at: indexPath) else { return }
+		guard collectionView.bounds.contains(cell.frame) else {
+			layout.animateBackwardScroll(to: indexPath.row)
+			return
+		}
+	}
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ViewController: UICollectionViewDataSource {
+	
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return cards.count
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell: CardCollectionViewCell = collectionView.dequeueCell(for: indexPath)
+		cell.fill(with: cards[indexPath.row])
+		return cell
+	}
+}
